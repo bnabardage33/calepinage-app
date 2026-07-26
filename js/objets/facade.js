962fc883333d8934f7ee77a13be3cb029d670e9f
bnabardage33 <bna.bardage33@gmail.com>
2026-07-26 22:35:15 +0200

    Refactor Facade class properties and methods

--- js/objets/facade.js
+++ js/objets/facade.js
@@ -1,5 +1,7 @@
 // ======================================
-// FACADE
+// CALEPINAGE PRO
+// FACADE.JS
+// Objet métier Bardage
 // ======================================
 
 class Facade {
@@ -10,15 +12,43 @@ class Facade {
 
         this.type = "facade";
 
+        // Géométrie
         this.x = x;
         this.y = y;
-
         this.width = width;
         this.height = height;
 
-        this.color = "#5d8fc2";
-
+        // Affichage
+        this.color = "#5D8FC2";
         this.selected = false;
+
+        // ==========================
+        // Métier Bardage
+        // ==========================
+
+        this.name = "Nouvelle façade";
+
+        this.manufacturer = "";
+
+        this.cladding = "";
+
+        this.orientation = "Vertical";
+
+        this.support = "Acier";
+
+        this.insulation = "";
+
+        this.panelWidth = 1000;
+
+        this.panelHeight = 0;
+
+        this.joint = 20;
+
+        this.finishings = [];
+
+        this.openings = [];
+
+        this.panels = [];
 
     }
 
@@ -29,69 +59,47 @@ class Facade {
         ctx.fillStyle = this.color;
 
         ctx.fillRect(
-
             this.x,
-
             this.y,
-
             this.width,
-
             this.height
-
         );
 
-        ctx.strokeStyle = "#1b1b1b";
-
+        ctx.strokeStyle = "#1A1A1A";
         ctx.lineWidth = 2 / engine.camera.zoom;
 
         ctx.strokeRect(
-
             this.x,
-
             this.y,
-
             this.width,
-
             this.height
-
         );
 
         if (this.selected) {
 
-            ctx.strokeStyle = "#00b7ff";
-
+            ctx.strokeStyle = "#00BFFF";
             ctx.lineWidth = 3 / engine.camera.zoom;
 
             ctx.strokeRect(
-
                 this.x,
-
                 this.y,
-
                 this.width,
-
                 this.height
-
             );
 
         }
 
     }
 
-    contains(x,y){
+    contains(x, y) {
 
+        return (
+            x >= this.x &&
+            x <= this.x + this.width &&
+            y >= this.y &&
+            y <= this.y + this.height
+        );
 
-    return (
-
-        x >= this.x &&
-
-        x <= this.x + this.width &&
-
-        y >= this.y &&
-
-        y <= this.y + this.height
-
-    );
-
+    }
 
 }
