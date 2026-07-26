3ec545c712c7b11cf4e56a232287ccacf779e200
bnabardage33 <bna.bardage33@gmail.com>
2026-07-26 21:17:52 +0200

    Create engine.js

---
+++ js/core/engine.js
@@ -0,0 +1,70 @@
+// =============================================
+// CALEPINAGE PRO
+// ENGINE V1
+// =============================================
+
+class Engine {
+
+    constructor(canvasId) {
+
+        this.canvas = document.getElementById(canvasId);
+        this.ctx = this.canvas.getContext("2d");
+
+        this.width = 0;
+        this.height = 0;
+
+        this.camera = {
+            x: 0,
+            y: 0,
+            zoom: 1
+        };
+
+        this.mouse = {
+            x: 0,
+            y: 0,
+            worldX: 0,
+            worldY: 0,
+            left: false,
+            middle: false,
+            right: false
+        };
+
+        this.objects = [];
+
+        this.running = false;
+
+    }
+
+    resize() {
+
+        this.width = this.canvas.parentElement.clientWidth;
+        this.height = this.canvas.parentElement.clientHeight;
+
+        this.canvas.width = this.width;
+        this.canvas.height = this.height;
+
+    }
+
+    start() {
+
+        this.running = true;
+
+        this.resize();
+
+        window.addEventListener("resize", () => this.resize());
+
+        this.loop();
+
+    }
+
+    loop() {
+
+        if (!this.running) return;
+
+        Renderer.draw(this);
+
+        requestAnimationFrame(() => this.loop());
+
+    }
+
+}
