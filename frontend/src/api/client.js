b6a614f864a232d21a993040094dc9dc4772dba7
bnabardage33 <bna.bardage33@gmail.com>
2026-07-28 23:27:41 +0200

    Create client.js

---
+++ frontend/src/api/client.js
@@ -0,0 +1,16 @@
+import axios from 'axios'
+
+export const api = axios.create({
+  baseURL: '/api',
+})
+
+export const getChantiers = () => api.get('/chantiers').then((r) => r.data)
+export const getChantier = (id) => api.get(`/chantiers/${id}`).then((r) => r.data)
+export const createChantier = (data) => api.post('/chantiers', data).then((r) => r.data)
+
+export const getClients = () => api.get('/clients').then((r) => r.data)
+export const createClient = (data) => api.post('/clients', data).then((r) => r.data)
+
+export const getFacades = (chantierId) =>
+  api.get(`/chantiers/${chantierId}/facades`).then((r) => r.data)
+export const createFacade = (data) => api.post('/facades', data).then((r) => r.data)
