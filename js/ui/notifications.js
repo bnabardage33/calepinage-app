40b179ae18f7604811b60bc2319d57427d6a86c0
bnabardage33 <bna.bardage33@gmail.com>
2026-07-28 05:50:36 +0200

    Refactor CalepinageGenerator to Notifications class

---
+++ js/ui/notifications.js
@@ -0,0 +1,25 @@
+// ==========================================
+// NOTIFICATIONS
+// ==========================================
+
+class Notifications{
+
+    success(message){
+
+        console.log("✅ " + message);
+
+    }
+
+    error(message){
+
+        console.error("❌ " + message);
+
+    }
+
+    info(message){
+
+        console.log("ℹ️ " + message);
+
+    }
+
+}
