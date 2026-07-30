2ffdb8798893e10e6a37dc6c57967e8117a06e90
bnabardage33 <bna.bardage33@gmail.com>
2026-07-30 19:50:30 +0200

    Update Vite config to set base path
    Set base path for relative script paths on Render

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: './', // <--- Indispensable pour que les chemins des scripts soient relatifs sur Render
   plugins: [react()],
   server: {
     port: 5173,
