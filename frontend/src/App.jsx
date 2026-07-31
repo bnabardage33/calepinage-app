6d7979e2d0dfc97d5b4098806e77186a116988cd
bnabardage33 <bna.bardage33@gmail.com>
2026-07-31 13:09:08 +0200

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,21 @@
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Layout from './components/Layout.jsx'
+import Dashboard from './pages/Dashboard.jsx'
 import Chantiers from './pages/Chantiers.jsx'
 import ChantierDetail from './pages/ChantierDetail.jsx'
+import Clients from './pages/Clients.jsx'
 
 export default function App() {
   return (
     <BrowserRouter>
-      <nav>
-        <Link to="/">Chantiers</Link>
-      </nav>
-      <Routes>
-        <Route path="/" element={<Chantiers />} />
-        <Route path="/chantiers/:id" element={<ChantierDetail />} />
-      </Routes>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/chantiers" element={<Chantiers />} />
+          <Route path="/chantiers/:id" element={<ChantierDetail />} />
+          <Route path="/clients" element={<Clients />} />
+        </Routes>
+      </Layout>
     </BrowserRouter>
   )
 }
